59a338ceee28a726016a1f0fde4e5172291b245c
Ahto Ahven <ahto.ahven@itcollege.ee>
2016-09-16 14:07:49 +0300

    test

---
+++ Tere.java
@@ -0,0 +1,9 @@
+ public class Tere {
+   public static void main (String[] args) {
+     System.out.println ("*****  *****  ****  *****   *");
+     System.out.println ("  *    *      *   * *       *");
+     System.out.println ("  *    *****  ***   *****   *");
+     System.out.println ("  *    *      * *   *        ");
+     System.out.println ("  *    *****  *   * *****   *");
+   }
+ }
